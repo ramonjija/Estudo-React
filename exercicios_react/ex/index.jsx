36ca9c7f88569f0d9fd015df53e2113296b835d4
Ramon Silva <ramon.silva@concretesolutions.com.br>
2018-04-24 14:35:31 -0300

    - Adição de babel transform pra spread

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import Component from './component'
 // import {Primeiro, Segundo} from './doisComponentes'
-import FamilySilva from './familySilva'
+// import FamilySilva from './familySilva'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
 <div>
-   <FamilySilva />
+    <Family lastName='Silva'>
+        <Member name='Ramon'/>
+    </Family>
 </div>, 
 document.getElementById('app'))
