3b238e3f2173bfbc584cb92a12e8dfdeb284e264
Ramon Silva <ramon.silva@concretesolutions.com.br>
2018-04-25 17:37:36 -0300

    - Finalização da configuração das rotas

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
 
 server.listen(port,function() {
     console.log(`Backend is running on port: ${port}. ^_^`);
-})
+});
+
+module.exports = server;
